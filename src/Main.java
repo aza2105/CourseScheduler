ee6873c85bcc21beaf45af459702130cfdf0248a
Tim Waterman <watermantium@gmail.com>
2014-06-21 16:50:32 -0400

    Parser updated with breadth info

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Parser parser = new Parser();
 		
-		parser.importData("security");
+		Parser parser = new Parser(Track.SECURITY);
+		parser.parseAll();
+		
 	}
 
 }
--- src/Parser.java
+++ src/Parser.java
@@ -5,15 +5,24 @@ import java.util.regex.*;
 public class Parser {
 	
 	private static ArrayList<Course> course;
+	private String track;
 	
-	public Parser() {
+	public Parser(String t) {
 		
 		course = new ArrayList<Course>();
+		track = t;
+	}
+	
+	public void parseAll() {
+		
+		importData(track);
+		parseBreadthRequirements();
+		
 	}
 	
 	public void importData(String track) {
 		
-		track = track + ".csv";
+		track = track + "Courses.csv";
 		
 		try {
 			BufferedReader input = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/" + track));
@@ -36,12 +45,49 @@ public class Parser {
 			
 		}
 		catch (Exception e) {
+			System.out.println(e);
 			System.exit(1);
 		}
 		
 	}
 	
-	public void parseRequirements(String text) {
+	public void parseBreadthRequirements() {
+		
+		try {	
+			BufferedReader input = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/" + "AllTrackReqs.csv"));
+			String line = null;
+			int whichGroup = 0;
+			int group1 = 0;
+			int group2 = 0;
+			int group3 = 0;
+			
+			
+			while( (line = input.readLine()) != null) {
+				
+				if(line.contains("group 1"))
+					whichGroup = 1;
+				if(line.contains("group 2"))
+					whichGroup = 2;
+				if(line.contains("group 3"))
+					whichGroup = 3;
+				if(whichGroup == 1) //We're counting Group 1
+					group1++;
+				if(whichGroup == 2)
+					group2++;
+				if(whichGroup == 3)
+					group3++;
+			}
+			
+			System.out.println(group1);
+			System.out.println(group2);
+			System.out.println(group3);
+			
+			
+		}
+		catch(Exception e) {
+			System.out.println(e);
+			System.exit(1);
+		}
 		
 		
 	}
