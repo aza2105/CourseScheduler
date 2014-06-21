7f2d2b3909484b6fe5409efe7651cf0b69b89b00
aza2105 <aza2105@columbia.edu>
2014-06-20 23:25:32 -0400

    Added skeleton for Scheduler

---
+++ src/Scheduler.java
@@ -0,0 +1,22 @@
+public class Scheduler
+{
+    /*
+     *
+     *function GRAPH-SEARCH(problem) returns a solution, or failure
+     *     initialize the frontier using the initial state of problem
+     *     initialize the explored set to be empty
+     *     loop do
+     *          if the frontier is empty then return failure
+     *          choose a leaf node and remove it from the frontier
+     *          if the node contains a goal state then return the corresponding solution
+     *          add the node to the explored set
+     *          expand the chosen node, adding the resulting nodes to the frontier
+     *             only if not in the frontier or explored set
+     *
+     *Russell, Peter Norvig Stuart (2009-12-01). Artificial Intelligence: A Modern Approach, 3/e (Page 77). Pearson HE, Inc.. Kindle Edition. 
+     *
+     */
+
+
+
+}
