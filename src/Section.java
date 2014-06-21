868064f2f06d7a8c3c977c9f280d41f9f694ce6a
Samuel Friedman <sf2636@columbia.edu>
2014-06-21 15:49:47 -0400

    added initial utility function

--- src/Section.java
+++ src/Section.java
@@ -5,15 +5,23 @@ public class Section extends Course {
     private int endTime;
     private int sectionNumber;
     private String instructor;
+    private Instructor inst;
 
 
     public Section(String t, String n, char a) {
     	super(t,n,a);
+    	//inst = new Instructor(String nugget, String firstName, String lastName, String middleName, int instructorID);
+    	inst = new Instructor(null, null, null, null, 0);
     }
     
     public String toString() {
     	
     	return super.toString();
+    }
+    
+    // Return the String value ("gold", "silver") for nugget
+    public String getNuggetValue(){
+    	return inst.getNugget();
     }
 
 }
--- src/Utility.java
+++ src/Utility.java
@@ -12,11 +12,37 @@ public class Utility {
 		totalUtility = 0;
 	}
 	
+	
+	// Return the utility value of a semester full of section objects
 	private static int checkUtility(ArrayList <Section> section){
 		/* 1 - Nuggets (40 - G, 30 - S, 0), Requirements (75), Wait Time (0 - 50)
 		 * 2 - % Enrollment Change (X), Probability of Course Offering (0 - 40)
 		 * 3 - Class Timing Preferences (30 - Day, 100 - Night, 0 - DC), # Courses to Take
 		 */
+		
+		// Initialize the starting utility value to 0
+		int tempUtil = 0;
+		
+		// Successively check and add the value of each sections utility to the total utility
+		// of the semester
+		for(int i = 0; i < section.size(); i++){
+			
+			// Initial value for the nugget value for each section
+			int nuggetVal = 0;
+			// Convert nugget String to a value
+			if((section.get(i).getNuggetValue()).equals("gold")){
+				nuggetVal = 40;
+			}
+			else if((section.get(i).getNuggetValue()).equals("silver")){
+				nuggetVal = 30;
+			}
+			
+			
+			tempUtil = tempUtil + nuggetVal;
+		}
+		
+		
+		
 		return totalUtility;
 		
 	}
