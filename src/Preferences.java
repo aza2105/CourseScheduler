6644af0716302c3cd5d0fe15585477da7b5ea49e
Samuel Friedman <sf2636@columbia.edu>
2014-06-20 23:27:41 -0400

    Updated preferences with number of courses per semester

--- src/Preferences.java
+++ src/Preferences.java
@@ -1,10 +1,23 @@
 
-// Class for user preference storage
+// Class for user preference storage.
 public class Preferences {
 
+	int [] numCoursesPerSem;
+	int dayNight; // 0 is day, 1 is night, 2 is don't care
+	
+	// Constructor to initialize user preference variables.
 	public Preferences(){
+		// Default courses per semester is 4-4-2
+		numCoursesPerSem = new int[10];
+		numCoursesPerSem[0] = 4;
+		numCoursesPerSem[1] = 4;
+		numCoursesPerSem[2] = 2;
 		
+		dayNight = 2; // Initially set for don't care
 	}
 	
-	
+	// Return the number of courses requested by the user per the given semester.
+	private int getNumCoursesPerSem(int generation){
+		return numCoursesPerSem[generation];
+	}
 }
