1f1eb1e97dd90d6dfc1366da34b0c4953e76449f
Samuel Friedman <sf2636@columbia.edu>
2014-06-21 16:53:50 -0400

    Trying to push main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		parseUserInput(args);
+		
 		Parser parser = new Parser(Track.SECURITY);
 		
 		parser.parseAll();
+	}
+	
+	private static void parseUserInput(String [] userInput){
+		if(userInput.length > 1){
+			System.out.println("Too many parameters. Please only include preferences file");
+		}
+		else if(userInput.length == 0){
+			Preferences prefs = new Preferences();
+		}
+		else if(userInput.length == 1){
+			String fileLocation = userInput[0];
+			String line = "";
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/" + fileLocation));
+				
+				while((line = br.readLine()) != null) {
+					
+				}
+			
+			} catch (FileNotFoundException e) {
+				System.out.println("File not found. Exiting program.");
+				System.exit(1);
+			}
+		}
 	}
 
 }
