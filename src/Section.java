afdb1ebe4d7fa4e47d01e8cafd60774b19d6cfa7
Tim Waterman <watermantium@gmail.com>
2014-06-20 22:58:39 -0400

    Section class add

---
+++ src/Section.java
@@ -0,0 +1,19 @@
+
+public class Section extends Course {
+
+    private int startTime;
+    private int endTime;
+    private int sectionNumber;
+    private String instructor;
+
+
+    public Section(String t, String n, char a) {
+    	super(t,n,a);
+    }
+    
+    public String toString() {
+    	
+    	return super.toString();
+    }
+
+}
