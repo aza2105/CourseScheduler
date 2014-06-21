19773afe7bc9942c1d78c4345c7c25994875f28d
Tim Waterman <watermantium@gmail.com>
2014-06-21 15:56:19 -0400

    Makefile and Track.java added, csv's moved for testing

---
+++ src/Track.java
@@ -0,0 +1,12 @@
+public class Track {
+
+    static String SECURITY = "CompSec";
+    static String COMPBIO = "CompBio";
+    static String FOUNDATIONS = "Foundations";
+    static String ML = "MachLearn";
+    static String NLP = "Nlp";
+    static String NETWORK = "NetSys";
+    static String SOFTWARE = "SoftSys";
+    static String VISION = "Vision";
+
+}
