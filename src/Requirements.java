cb2b87bbd7a6da8c8f13c7c93fd71107ba988ef6
Tim Waterman <watermantium@gmail.com>
2014-06-22 14:30:20 -0400

    Parsing of Requirements

--- src/Requirements.java
+++ src/Requirements.java
@@ -2,16 +2,67 @@ import java.util.*;
 
 public class Requirements {
 	
+	private static ArrayList<Course> requiredCourses = new ArrayList<Course>();
+	private static LinkedList<Course> chooseBetween = new LinkedList<Course>();
+	private int chooseBetweenNum;
+	
 	private int req6000;
 	private int creditsRequired;
-	private LinkedList<Course> corerequirements;
-	private LinkedList<Course> breadthrequirements;
+	//private LinkedList<Course> corerequirements;
+	//private LinkedList<Course> breadthrequirements;
 	
 	
 	boolean requirementsFullfilled;
 	
 	public Requirements() {
 		
+	}
+	
+	public void clearAllReqs() {
+		
+		requiredCourses.clear();
+		chooseBetween.clear();
+	}
+	
+	public void setChooseBetweenNum(int i) {
+		
+		chooseBetweenNum = i;
+	}
+	
+	public void addRequiredClass(Course course) {
+		
+		requiredCourses.add(course);
+	}
+	
+	public boolean searchRequiredCourses(Course course) {
+		
+		if (requiredCourses.contains(course))
+			return true;
+		return false;
+	}
+	
+	public boolean searchLooseRequirements(Course s) {
+		
+		if (chooseBetween.contains(s))
+			return true;
+		return false;
+	}
+	
+	public void addLooseRequirement(Course course) {
+		
+		chooseBetween.add(course);
+	}
+	
+	public void printRequirements() {
+		//print requirements
+		System.out.println("The following are required: ");
+		for(Course c : requiredCourses)
+			System.out.println(c);
+		
+		//print out the loose(r) requirements
+		System.out.println("Choose " + chooseBetweenNum + " of the following: ");
+		for(Course c : chooseBetween)
+			System.out.println(c);
 	}
 	
 	public boolean reqsMet(LinkedList<Course> taken) {
