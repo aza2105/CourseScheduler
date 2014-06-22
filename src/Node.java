a853d501088da2bce13ff0a3d43ce6ea914accf8
aza2105 <aza2105@columbia.edu>
2014-06-22 14:11:35 -0400

    Updated Node and Semester

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
+
 public class Node 
 {
+	private Semester semester;
+	
+	//the previous semester
+	private Node parentNode;
+	
+	// provide a textual representation of a Node object
+	public String toString()
+	{
+		if (semester != null)
+		{
+			LinkedList<sections> semesterSections = semester.getSections();
+			for (Section s : semesterSections)
+			{
+				System.out.print(s);
+			}
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	public Semester getSemester()
+	{
+		return semester;
+	}
+	
+	public void setSemester(Semester semester)
+	{
+		this.semester = semester;
+	}
+	
+	public Node getParentNode()
+	{
+		return parentNode;
+	}
 
+	public void setParentNode(Node parentNode)
+	{
+		this.parentNode = parentNode;
+	}
+	
 }
--- src/Semester.java
+++ src/Semester.java
@@ -1,14 +1,40 @@
 import java.util.*;
 
-public class Semester {
+/*
+ * Semester here refers to a proposed semester schedule
+ * So, for instance a proposed schedule for Fall 2014 would be 
+ * considered an instance of Semester. A different proposed schedule
+ * for Fall 2014 would be a different Semester instance.
+ */
+
+public class Semester 
+{
 
     private LinkedList<Section> sections;
+    
+    //depth = distance from origin semester
+    private int depth;
+    
     private int MAXSIZE;
 
-    public Semester(int i) {
+    public Semester(int i) 
+    {
 
         MAXSIZE = i;
         sections = new LinkedList<Section>();
     }
+    
+    public int getDepth 
+    {
+    	return depth;
+    }
+    
+    public void setSections()
+    public LinkedList<Section> getSections() 
+    {
+    	return sections;
+    }
+    
+    
 
 }
