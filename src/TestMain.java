0e0fcaff9fc53f54cc0e53b4fd8e0a1707452cd9
Tim Waterman <watermantium@gmail.com>
2014-06-27 18:39:49 -0400

    Images added and TestMain changed

--- src/TestMain.java
+++ src/TestMain.java
@@ -9,10 +9,11 @@ public class TestMain {
 
         LinkedList<Course> test = new LinkedList<Course>();
 
-        test.add(new Course("Op Sys", "COMS W4115", 'w'));
-        test.add(new Course("ASE", "COMS W4119", 'w'));
-        test.add(new Course("NetSec", "COMS W4111", 't'));
+        test.add(new Course("Op Sys", "COMS W4118", 'w'));
+        test.add(new Course("ASE", "COMS W4180", 'w'));
+        test.add(new Course("NetSec", "COMS W6118", 't'));
 
+        test.add(new Course("NetSec", "COMS W4261", 't'));
         System.out.println(Parser.reqs.rulesUnmet(test));
 
     }
