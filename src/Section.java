c36ec62316434b9968d49ebb701c5946dfdccea2
Martin Wren <wren@columbia.edu>
2014-06-27 18:22:09 -0400

    updates to section object to be created by historical parser called from scheduler

--- src/Section.java
+++ src/Section.java
@@ -1,18 +1,46 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
-public class Section extends Course {
+public class Section  {
 
-    private int startTime;
-    private int endTime;
+	private String daySchedule;
+    private Date startTime;
+    private Date endTime;
     private int sectionNumber;
     private String instructor;
     private Instructor inst;
 
+    private Course parent;
+    
+    public Section(Course c, String days, String start, String end, String pLast, String pFirst, String pMiddle ) {
+    	
+    	this.parent = c;
+    	this.daySchedule = days;
+//    	this.startTime = start;
+//    	this.endTime = end;
 
-    public Section(String t, String n, char a) {
+    	SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
+    	
+    	start = new StringBuilder(start).insert(start.length()-2, ":").toString();
+    	end = new StringBuilder(end).insert(end.length()-2, ":").toString();
+
+    	try {
+    		this.startTime = sdf.parse( start ); //new SimpleDateFormat( start );
+    		this.endTime = sdf.parse( end );//new SimpleDateFormat( end );
+    	} catch ( ParseException e ) {
+    	
+    	}
+    	
+    	inst = Instructor.findInstructor( pFirst, pMiddle, pLast );
+    	
+    }
+
+/*    public Section(String t, String n, char a) {
     	super(t,n,a);
     	//inst = new Instructor(String nugget, String firstName, String lastName, String middleName, int instructorID);
     	inst = new Instructor(null, null, null, null, 0);
-    }
+    } */
     
     public String toString() {
     	
@@ -24,4 +52,17 @@ public class Section extends Course {
     	return inst.getNugget();
     }
 
+    public Date getStart() {
+    
+    	return startTime;
+    	
+    }
+    
+    public Date getEnd() {
+    	
+    	return endTime;
+    	
+    }
+    
+    
 }
