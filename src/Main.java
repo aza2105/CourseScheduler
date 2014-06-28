249284f17c16eabad5ae27418215143c48732b5d
Samuel Friedman <sf2636@columbia.edu>
2014-06-27 22:34:20 -0400

    Moved Schedule Display contents to main, removed FrontEnd's main method and put necessary code in Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
+import java.awt.EventQueue;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
+
+import javax.swing.JLabel;
 
 
 public class Main {
-	
+	private ArrayList<JLabel> labels;
+	private static final int HEIGHT = 800;
+	private static final int WIDTH = 650;
+	private final int X_START = 60;
+	private final int Y_START = 50;
+	private static int OFFSET = 200;
+	private static int VERTOFF = 150;
+	private static int MAXWIDTH = 1000;
+	private static int MAXHEIGHT = 1000;
 	// Preferences object to store the user preferences
 	//static Preferences prefs;
 	
@@ -19,7 +32,11 @@ public class Main {
 	
 	// Main method
 	public static void main(String[] args) {
-
+		timTest();
+		// Starts the GUI Front of FrontEnd
+		FrontEnd frame = new FrontEnd();
+		frame.setVisible(true);
+		
 		// A maximum of one parameter is allowed (the file location/name).
 		if(args.length > 1){
 			System.out.println("Too many parameters. Please only include preferences file.");
@@ -68,4 +85,60 @@ public class Main {
         System.out.println(Utility.getUtility(sectionList));
         */
 	}	
+	
+	public static void timTest(){
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					
+					/*LinkedList<Course> schedule = new LinkedList<Course>();
+					schedule.add(new Course("Class One", "COMS 4701"));
+					schedule.add(new Course("Class Two", "COMS 4702"));
+					schedule.add(new Course("Class Three", "COMS 4703"));
+					schedule.add(new Course("Class Four", "COMS 4704"));
+					Set<Course> set1 = new HashSet<Course>(schedule);
+					Semester first = new Semester(0,0,2014,set1,null,set1);
+					
+					LinkedList<Course> schedule2 = new LinkedList<Course>();
+					schedule2.add(new Course("Class Five", "COMS 4705"));
+					schedule2.add(new Course("Class Six", "COMS 4706"));
+					schedule2.add(new Course("Class Seven", "COMS 4707"));
+					schedule2.add(new Course("Class Eight", "COMS 4708"));
+					schedule2.add(new Course("Class Five", "COMS 4705"));
+					schedule2.add(new Course("Class Six", "COMS 4706"));
+					schedule2.add(new Course("Class Seven", "COMS 4707"));
+					schedule2.add(new Course("Class Eight", "COMS 4708"));
+					Set<Course> set2 = new HashSet<Course>(schedule2);
+					Semester second = new Semester(0,1,2015,set2,null,set1);
+					
+					LinkedList<Course> schedule3 = new LinkedList<Course>();
+					schedule3.add(new Course("Class Nine", "COMS 4709"));
+					schedule3.add(new Course("Class Ten", "COMS 4710"));
+					Set<Course> set3 = new HashSet<Course>(schedule3);
+					Semester third = new Semester(0,0,2015,set3,null,set1);
+					
+					LinkedList<Semester> mysemesters = new LinkedList<Semester>();
+					mysemesters.add(first);
+					mysemesters.add(second);
+					mysemesters.add(third);
+	
+					
+					if (mysemesters.size() < 6)
+						OFFSET = WIDTH / mysemesters.size();
+					else
+						OFFSET = MAXWIDTH / mysemesters.size();
+					
+					VERTOFF = HEIGHT / (8);*/
+					
+					// What Scheduler will need to call!
+					//ScheduleDisplay frame = new ScheduleDisplay(mysemesters);
+					//frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
