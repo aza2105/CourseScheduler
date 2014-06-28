2c6617fe92bafa9e2eaffb5253fcf91ed96f32f0
Tim Waterman <watermantium@gmail.com>
2014-06-27 20:24:38 -0400

    Output GUI scaled properly and scrolling

--- src/ScheduleDisplay.java
+++ src/ScheduleDisplay.java
@@ -4,23 +4,62 @@ import java.io.*;
 import java.util.*;
 
 import javax.swing.*;
+import javax.swing.border.*;
 
 
 public class ScheduleDisplay extends JFrame {
 	
 	private ArrayList<JLabel> labels;
-	JPanel schedule;
-	JPanel schedule1; 
-	JPanel schedule2; 
-	JPanel schedule3; 
-	JPanel schedule4; 
-	JPanel schedule5; 
+	private static final int HEIGHT = 800;
+	private static final int WIDTH = 650;
+	private final int X_START = 60;
+	private final int Y_START = 50;
+	private static int OFFSET = 200;
+	private static int HORZOFF = 150;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ScheduleDisplay frame = new ScheduleDisplay();
+					
+					
+					LinkedList<Course> schedule = new LinkedList<Course>();
+					schedule.add(new Course("Class One", "COMS 4701"));
+					schedule.add(new Course("Class Two", "COMS 4702"));
+					schedule.add(new Course("Class Three", "COMS 4703"));
+					schedule.add(new Course("Class Four", "COMS 4704"));
+					Set<Course> set1 = new HashSet<Course>(schedule);
+					Semester first = new Semester(0,0,2014,set1,null,set1);
+					
+					LinkedList<Course> schedule2 = new LinkedList<Course>();
+					schedule2.add(new Course("Class Five", "COMS 4705"));
+					schedule2.add(new Course("Class Six", "COMS 4706"));
+					schedule2.add(new Course("Class Seven", "COMS 4707"));
+					schedule2.add(new Course("Class Eight", "COMS 4708"));
+					schedule2.add(new Course("Class Five", "COMS 4705"));
+					schedule2.add(new Course("Class Six", "COMS 4706"));
+					schedule2.add(new Course("Class Seven", "COMS 4707"));
+					schedule2.add(new Course("Class Eight", "COMS 4708"));
+					Set<Course> set2 = new HashSet<Course>(schedule2);
+					Semester second = new Semester(0,1,2015,set2,null,set1);
+					
+					LinkedList<Course> schedule3 = new LinkedList<Course>();
+					schedule3.add(new Course("Class Nine", "COMS 4709"));
+					schedule3.add(new Course("Class Ten", "COMS 4710"));
+					Set<Course> set3 = new HashSet<Course>(schedule3);
+					Semester third = new Semester(0,0,2015,set3,null,set1);
+					
+					LinkedList<Semester> mysemesters = new LinkedList<Semester>();
+					mysemesters.add(first);
+					mysemesters.add(second);
+					mysemesters.add(third);
+					mysemesters.add(second);
+					mysemesters.add(third);
+					
+					OFFSET = WIDTH / mysemesters.size();
+					HORZOFF = HEIGHT / (8);
+					
+					ScheduleDisplay frame = new ScheduleDisplay(mysemesters);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -29,146 +68,78 @@ public class ScheduleDisplay extends JFrame {
 		});
 	}
 
-    public ScheduleDisplay() {
+    public ScheduleDisplay(LinkedList<Semester> semesters) {
+    	
+    	ImageIcon icon = new ImageIcon(System.getProperty("user.dir") + "/../images/cap.png");
     	
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	this.setSize(1000, 1000);
-    	getContentPane().setLayout(new BorderLayout(0, 0));
+    	this.setSize(HEIGHT, WIDTH);
+    	getContentPane().setLayout(null);
     	
-    	JPanel header = new JPanel();
-    	getContentPane().add(header, BorderLayout.NORTH);
+    	JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+    	tabbedPane.setBounds(0, 0, HEIGHT, WIDTH);
+    	getContentPane().add(tabbedPane);
     	
-    	JButton first = new JButton("Schedule 1");
-    	first.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent arg0) {
-    			schedule2.setVisible(false);
-    			schedule3.setVisible(false);
-    			schedule4.setVisible(false);
-    			schedule5.setVisible(false);
-    			
-    			schedule1.setVisible(true);
-    			
-    		}
-    	});
-    	header.add(first);
+    	JPanel firstschedule = new JPanel();
+    	firstschedule.setPreferredSize(new Dimension(1200,1200));
+    	//tabbedPane.addTab("Schedule 1", icon, firstschedule, null);
+    	firstschedule.setLayout(null);
     	
-    	JButton second = new JButton("Schedule 2");
-    	second.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent e) {
-    			schedule1.setVisible(false);
-    			schedule3.setVisible(false);
-    			schedule4.setVisible(false);
-    			schedule5.setVisible(false);
-    			
-    			schedule2.setVisible(true);
-    		}
-    	});
-    	header.add(second);
+    	JScrollPane myscroll = new JScrollPane(firstschedule);
+    	tabbedPane.addTab("Schedule 1", icon, myscroll, null);
+    	/*
+    	JScrollBar scrollBar = new JScrollBar();
+    	scrollBar.setUnitIncrement(50);
+    	scrollBar.setBounds(758, 6, 15, 574);
     	
-    	JButton third = new JButton("Schedule 3");
-    	third.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent e) {
-    			schedule2.setVisible(false);
-    			schedule1.setVisible(false);
-    			schedule4.setVisible(false);
-    			schedule5.setVisible(false);
-    			
-    			schedule3.setVisible(true);
-    		}
-    	});
-    	header.add(third);
+    	firstschedule.add(scrollBar);
+    	*/
+    	drawSchedule(semesters, firstschedule);
     	
-    	JButton fourth = new JButton("Schedule 4");
-    	fourth.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent e) {
-    			schedule2.setVisible(false);
-    			schedule3.setVisible(false);
-    			schedule1.setVisible(false);
-    			schedule5.setVisible(false);
-    			
-    			schedule4.setVisible(true);
-    		}
-    	});
-    	header.add(fourth);
+    	JPanel secondschedule = new JPanel();
+    	tabbedPane.addTab("Schedule 2", icon, secondschedule, null);
     	
-    	JButton fifth = new JButton("Schedule 5");
-    	fifth.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent e) {
-    			schedule2.setVisible(false);
-    			schedule3.setVisible(false);
-    			schedule4.setVisible(false);
-    			schedule1.setVisible(false);
-    			
-    			schedule5.setVisible(true);
-    		}
-    	});
-    	header.add(fifth);
+    	JPanel thirdschedule = new JPanel();
+    	tabbedPane.addTab("Schedule 3", icon, thirdschedule, null);
     	
-    	schedule = new JPanel();
-    	getContentPane().add(schedule, BorderLayout.CENTER);
-    	schedule.setLayout(new CardLayout(0, 0));
+    	JPanel fourthschedule = new JPanel();
+    	tabbedPane.addTab("Schedule 4", icon, fourthschedule, null);
     	
-    	schedule1 = new JPanel();
-    	schedule.add(schedule1, "name_1403839034874300000");
-    	schedule1.setLayout(new GridLayout(5, 5, 2, 2));
-    	schedule1.setVisible(false);
-    	
-    	schedule2 = new JPanel();
-    	schedule.add(schedule2, "name_1403839070891874000");
-    	schedule2.setLayout(new GridLayout(5, 5, 2, 2));
-    	schedule2.setVisible(false);
-    	
-    	schedule3 = new JPanel();
-    	schedule.add(schedule3, "name_1403839075299794000");
-    	schedule3.setLayout(new GridLayout(5, 5, 2, 2));
-    	schedule3.setVisible(false);
-    	
-    	schedule4 = new JPanel();
-    	schedule.add(schedule4, "name_1403839078881045000");
-    	schedule4.setLayout(new GridLayout(5, 5, 2, 2));
-    	schedule4.setVisible(false);
-    	
-    	schedule5 = new JPanel();
-    	schedule.add(schedule5, "name_1403839081129235000");
-    	schedule5.setLayout(new GridLayout(5, 5, 2, 2));
-    	schedule5.setVisible(false);
-
-    	
-    	addBoxes(schedule1);
-    	addBoxes(schedule2);
-    	addBoxes(schedule3);
-    	addBoxes(schedule4);
-    	addBoxes(schedule5);
+    	JPanel fifthschedule = new JPanel();
+    	tabbedPane.addTab("Schedule 5", icon, fifthschedule, null);
     }
     
-    public void addBoxes(JPanel panel) {
+    public void drawSchedule(LinkedList<Semester> semesters, JPanel panel) {
     	
-    	labels = new ArrayList<JLabel>();
-    	String s = "";
+    	ArrayList<JLabel> mylabels = new ArrayList<JLabel>();
     	
-    	if (panel.equals(schedule1))
-    		s = "ONE";
-    	if (panel.equals(schedule2))
-    		s = "TWO";
-    	if (panel.equals(schedule3))
-    		s = "THREE";
-    	if (panel.equals(schedule4))
-    		s = "FOUR";
-    	if (panel.equals(schedule5))
-    		s = "FIVE";
-    	
-    	for(int i = 0; i < 25; i++) {
-    		labels.add( new JLabel(s) );
-    		panel.add(labels.get(i));
-    		labels.get(i).setOpaque(true);
+    	for(int i = 0; i < semesters.size(); i++) {
     		
-    		if (i % 5 == 0 || i % 5 == 2 || i % 5 == 4) {
-    			labels.get(i).setBackground(new Color(235,253,253));
-    		}
-    		else {
-    			labels.get(i).setBackground(new Color(153,204,255));
+    		String term = "";
+    		if (semesters.get(i).getSemesterID() == 0)
+    			term = "FALL";
+    		else
+    			term = "SPRING";
+    		
+    		JLabel label = new JLabel(term + "  " + Integer.toString(semesters.get(i).getSemesterYear()) );
+    		label.setBounds(X_START + i*OFFSET, Y_START, 100, 40);
+    		label.setBorder(BorderFactory.createLineBorder(Color.black));
+    		mylabels.add(label);
+    		panel.add(label);
+    		
+    		Set<Course> sections = new HashSet<Course>(semesters.get(i).getSections());
+    		
+    		int j = 1;
+    		for(Course c : sections) {
+    			
+    			JLabel course = new JLabel(c.getID());
+    			course.setBounds(X_START + i*OFFSET, Y_START + j*HORZOFF, 90, 45);
+    			panel.add(course);
+    			j++;
     		}
     		
     	}
+    	
+    	
     }
 }
