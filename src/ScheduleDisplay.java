1552e4dd824ea285be7aaa488f2663e1e7f6f282
Tim Waterman <watermantium@gmail.com>
2014-06-28 13:39:10 -0400

    Consolidated into one Main function

--- src/ScheduleDisplay.java
+++ src/ScheduleDisplay.java
@@ -20,58 +20,13 @@ public class ScheduleDisplay extends JFrame {
 	private static int MAXWIDTH = 1000;
 	private static int MAXHEIGHT = 1000;
 	private ImageIcon icon;
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					
-					Set<Course> co = new HashSet<Course>();
-					co.add(new Course("FUCK", "THIS"));
-					Instructor.parseUserInput("instructors.csv");
-					
-					LinkedList<Section> schedule = new LinkedList<Section>();
-					schedule.add(new Section (new Course("Class One", "COMS 4701"),"MWF", "2200", "2330", "Friedman", "Samuel1", "C"));
-					schedule.add(new Section (new Course("Class One", "COMS 4701"),"MWF", "2200", "2330", "Friedman", "Samuel2", "C"));
-					schedule.add(new Section (new Course("Class One", "COMS 4701"),"MWF", "2200", "2330", "Friedman", "Samuel3", "C"));
-					schedule.add(new Section (new Course("Class One", "COMS 4701"),"MWF", "2200", "2330", "Friedman", "Samuel4", "C"));
-					Set<Section> set1 = new HashSet<Section>(schedule);
-					Semester first = new Semester(0,0,2014,set1,null,co,0);
-					
-					LinkedList<Section> schedule2 = new LinkedList<Section>();
-					schedule2.add(new Section (new Course("Class Five", "COMS 4705"),"MWF", "2200", "2330", "Friedman", "Samuel5", "C"));
-					schedule2.add(new Section (new Course("Class Six", "COMS 4706"),"MWF", "2200", "2330", "Friedman", "Samuel6", "C"));
-					schedule2.add(new Section (new Course("Class Seven", "COMS 4707"),"MWF", "2200", "2330", "Friedman", "Samuel7", "C"));
-					schedule2.add(new Section (new Course("Class Eight", "COMS 4708"),"MWF", "2200", "2330", "Friedman", "Samuel8", "C"));
-					schedule2.add(new Section (new Course("Class Five", "COMS 4705"),"MWF", "2200", "2330", "Friedman", "Samuel9", "C"));
-					schedule2.add(new Section (new Course("Class Six", "COMS 4706"),"MWF", "2200", "2330", "Friedman", "Samuel10", "C"));
-					schedule2.add(new Section (new Course("Class Seven", "COMS 4707"),"MWF", "2200", "2330", "Friedman", "Samuel21", "C"));
-					Set<Section> set2 = new HashSet<Section>(schedule2);
-					Semester second = new Semester(0,1,2015,set2,null,co,0);
-					
-					LinkedList<Semester> mysemesters = new LinkedList<Semester>();
-					mysemesters.add(first);
-					mysemesters.add(second);
-					mysemesters.add(second);
-					mysemesters.add(first);
-					mysemesters.add(second);
-					mysemesters.add(first);
-					ScheduleDisplay frame = new ScheduleDisplay();
-					frame.giveSchedule(mysemesters, 1);
-					frame.setVisible(true);
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
+	
     public ScheduleDisplay() {
     	
     	icon = new ImageIcon(System.getProperty("user.dir") + "/../images/cap.png");
     	
-    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     	this.setSize(HEIGHT, WIDTH);
     	getContentPane().setLayout(null);
     	
