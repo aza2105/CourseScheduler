5409eedea126e6dff48eec9cba442cad04a33c32
Martin Wren <wren@columbia.edu>
2014-07-04 18:09:46 -0400

    A star search version

---
+++ src/AScheduler.java
@@ -0,0 +1,370 @@
+/**
+ * Authors: Abdullah Al-Syed, Sam Friedman, Tim Waterman, Martin Wren
+ * Date: 7/3/14
+ * 
+ * Title: Scheduler.java
+ * 
+ * Scheduler objects are called upon to build the required course information
+ * for finding schedules and completing the actual traversal over node Semester
+ * objects.  
+ * 
+ * It builds an array of Directories of Classes accessible to the Semester objects
+ * at child generation and maintains a stack of nodes for depth-first behavior, 
+ * although each node, when returning its children, returns them sorted by lowest
+ * cost to highest cost.  These are placed on the stack highest cost first to 
+ * prioritize low cost traversals.
+ * 
+ * 
+ */
+import java.util.*;
+
+public class AScheduler 
+{
+
+	/*
+	 *	function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
+	 *		node <- a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
+	 *		frontier <- a priority queue ordered by PATH-COST, with node as the only element
+	 *		explored <- an empty set
+	 *		loop do
+	 *			if EMPTY?(frontier) then return failure
+	 *			node <- POP(frontier ) // chooses the lowest-cost node in frontier 
+	 *			if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
+	 *			add node.STATE to explored
+	 *			for each action in problem.ACTIONS(node.STATE) do
+	 *				child <- CHILD-NODE(problem, node, action)
+	 *				if child.STATE is not in explored or frontier then
+	 *					frontier <- INSERT(child,frontier )
+	 *				else if child.STATE is in frontier with higher PATH-COST then
+	 *					replace that frontier node with child
+	 *
+	 *Russell, Peter Norvig Stuart (2009-12-01). Artificial Intelligence: A Modern Approach, 3/e (Page 84). Pearson HE, Inc.. Kindle Edition. 
+	 * 
+	 */
+   
+	/*
+	 * CONSTANTS
+	 */
+//	private static final int MAX_DEPTH = 3;
+//	private static final int MAX_VALIDS = 5000;
+//	private static final double COST_CUTOFF = 320;
+	private static int TOTAL_NUM_COURSES_TO_TAKE = 10;
+//	private static boolean random = true;
+	
+	// keep track of the initial term
+	private int startSeason;
+	private int startYear;
+
+	// frontier for uniform evaluated search
+	PriorityQueue<ASemester> frontier = new PriorityQueue<ASemester>();
+
+	// an active semester object
+	private ASemester activeSemester;
+	
+	// number of semesters to consider
+	private int semesters;
+
+	// number of total terms to generate (max depth)
+	private int maxDepth;
+	
+	// number of courses to simulate
+	private int numCourses;
+
+	private static int[] numberOfCourses;
+	
+	private static int currentTerm;
+
+	// breakpoints, number of courses at which to begin a new semester
+	//   initialized with a zero
+	public static HashSet<Integer> semesterBreakpoints = new HashSet<Integer>();
+	
+	// List of Sets representing course offerings per term in the future
+	protected static ArrayList<Set<Section>> directoryOfClasses;
+
+//	private ArrayList<PriorityQueue<Semester>> sortedClasses = new ArrayList<PriorityQueue<Semester>>();
+//	private PriorityQueue<Semester> addToSorted = new PriorityQueue<Semester>();
+	
+	private ArrayList<Course> coursesToIgnore = new ArrayList<Course>();
+	
+	// Courses input as already completed
+	private Set<Course> rootInheritedCourses = new HashSet<Course>();
+	private int prevCourses = 0;
+	
+	// hash map for getting course information and generating potential schedules
+	private HashMap<String,Course> courses;
+
+	// set of valid candidates
+	private Set<Section> coursePool = new HashSet<Section>();	
+
+	private int currentDepth = 0;
+	private int keySize;
+	
+	// default constructor
+	public AScheduler() 
+	{ 
+		semesters = 2;   // spring and fall
+		
+		// get the number of semesters to consider
+		maxDepth = Preferences.prefs.getNumSems();
+
+		numCourses = Preferences.prefs.getTotalCourses() - Preferences.prefs.coursesTaken.size();
+	
+		System.out.println( "Generating "+numCourses+" courses over "+maxDepth+" semesters");
+		
+		// add course number breakpoints so we know when it's a new semester
+		semesterBreakpoints.add( Integer.valueOf( 0 ));
+		int lastadd = 0;
+		for ( int x=0; x<maxDepth; x++ ) {
+			semesterBreakpoints.add( Integer.valueOf( lastadd + Preferences.prefs.getNumCoursesPerSem( x )+1));
+			System.out.println( "added breakpoint at "+(Preferences.prefs.getNumCoursesPerSem( x )+lastadd+1));
+			lastadd += Preferences.prefs.getNumCoursesPerSem( x );
+		}
+
+		// set the term for the initial semester
+		startSeason = Preferences.prefs.getFirstSeason();
+		startYear = Preferences.prefs.getFirstYear();
+		
+		// grab a hash of all potential courses, regardless of specific
+		//   semester data
+		courses = new HashMap<String,Course>(); 
+		courses = HistoricalData.parseUserInput( "historical.csv" );
+
+		/* Course Requirement Utility setting */
+		for(Rule r : Parser.reqs.getRules() ) {
+			for( Course c : courses.values() ) {
+				if ( r.getCourseList().contains( c ) ) {
+					double ru; // determine the "antiutility"				
+					ru = Math.log(r.size() )/Math.log(2);
+					c.setRequired(ru);
+				}
+			}
+		}
+	
+
+		// set the courses to be inherited by root node
+		//   ( courses taken previously )
+		for ( String s : Preferences.prefs.getCoursesTaken() ) {
+			System.out.println( " Adding "+s+"to root courses");
+			rootInheritedCourses.add( courses.get( s ));
+			prevCourses++;
+		}
+
+		// Map actually offered courses to our generic Course objects
+		coursePool = HistoricalData.parseKnownInput( "known.csv", courses );
+
+		// dOC contains sets of actually offered classes per term
+		directoryOfClasses = new ArrayList<Set<Section>>();
+		
+		// keep track of year and semester when building
+		int xYear = startYear;
+		int xSeason = startSeason;
+		
+		if ( !(coursePool == null) ) {
+
+			// assuming first sem to be fall 2014
+			// add the known offerings to the dOC
+			directoryOfClasses.add(0, coursePool); 
+
+			currentTerm = 0;
+
+			// increment term logically
+			if (xSeason == 0) {
+	    		xSeason = 1;
+	    		xYear++;
+	    	}
+	    	//else if current semester is a spring semester
+	    	else if (xSeason == 1)  {
+	    		xSeason = 0;
+	    	}			
+		}
+		else {
+			System.err.println( "coursePool is null");
+			System.exit(1);
+		}
+
+		// generate directories of classes
+		for ( int i=1; i<maxDepth; i++ ) {
+
+			System.out.println( "Generating DoC for depth "+i+ ", max "+maxDepth);
+
+			// get a set of hypothetical course section offerings from TEH FUTURES
+			HashSet<Section> poolOfCoursesForChildSemesters = new HashSet<Section>();
+
+			// for each of the courses defined, find if they will likely be 
+			//  offered for each semester we are considering
+			for ( Course c : courses.values() ) {
+				if ( c.probOffered( String.valueOf(xSeason), String.valueOf(xYear) ) ) {
+					poolOfCoursesForChildSemesters.add( new Section( c, null, null, null, null, null, null ));
+				}								
+			}
+
+			// add pool of sections to the dOC
+			directoryOfClasses.add( poolOfCoursesForChildSemesters );
+			
+			
+	    	if (xSeason == 0)  	{
+	    		xSeason = 1;
+	    		xYear++;
+	    	}
+	    	//else if current semester is a spring semester
+	    	else if (xSeason == 1)	{
+	    		xSeason = 0;
+	    	}
+		}		
+		
+	}	
+		
+
+
+	// Here is where the traversal happens.  It is now DFS with an evaluation
+	//  function, not exactly uniform cost
+	public LinkedList<ASemester> uniformCostSearch()
+	{
+		LinkedList<ASemester> optimalSemesterList = new LinkedList<ASemester>();
+
+		if ( directoryOfClasses == null ) {
+			System.err.println( "Error: directoryOfClasses does not exist");
+			System.exit(1);
+		}
+
+		// instantiate the root semester, which contains any courses previously
+		//  taken, considered as one semester
+		ASemester sem = new ASemester(0, -1, 0, 2014, null, null, rootInheritedCourses, 0.0, 0.0);
+
+		// set the root to active
+		activeSemester = sem;
+
+		// maintain explored?
+		Set<ASemester> explored = new HashSet<ASemester>();
+
+		// add the root to the frontier
+		frontier.offer( sem );
+
+		// track current depth
+		currentDepth = 0;
+
+		// nodes explored
+		int nodeCount = 1;
+
+		// keep track of time
+		long startTime = System.nanoTime();
+		
+		ArrayList<ASemester> childSem = new ArrayList<ASemester>();
+		
+		while (true)
+		{
+
+			// failure condition. optimal not found and nothing to observe
+			if (frontier.isEmpty())
+			{
+				
+				optimalSemesterList = null; //returning failure
+				System.out.println( "Unable to find a valid schedule.");
+		
+				break;
+			}
+							
+				
+				// we have reached a goal state.  return a linked list of semester
+				//  objects for output and user feedback
+				if (succeedsGoalTest(activeSemester))
+				{
+					optimalSemesterList.addFirst(activeSemester);
+					
+					//backtrack up to the root to get the schedules for each semester
+					//along the path from the root to the goal semester
+
+					System.out.println( "Semester Found");
+					
+					optimalSemesterList = activeSemester.getFinal();
+					
+					for( int x = 0; x < optimalSemesterList.size(); x++ ) {
+						System.out.println( optimalSemesterList.get(x));
+					}	
+					
+					System.out.println( " Nodes explored: "+nodeCount);
+					System.out.println( " Time taken: "+(( System.nanoTime() - startTime ) / 1000000000 )+"s");
+					
+					break;
+				}
+				
+				
+
+				// if we're not at a goal state
+				if (activeSemester.getDepth() <= numCourses )		{
+
+					childSem = activeSemester.addChild();
+					
+					ArrayList<ASemester>temp = new ArrayList<ASemester>();
+					
+					// add new children to the frontier
+					for ( int i=0; i<childSem.size(); i++ ) {
+						frontier.offer( childSem.get(i) );						
+					}
+
+					nodeCount++;
+					activeSemester = frontier.poll();
+					
+				}
+				
+				
+		}		
+		
+		
+		return optimalSemesterList;
+		
+		
+	}
+	
+	private boolean succeedsGoalTest(ASemester semester)
+	{
+
+		// if we've hit the max depth, we've finished the last semester
+		//   optimally and can return.
+		if ( semester.getDepth() == numCourses ) {
+
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	
+	
+	/*
+	 * METHODS
+	 */
+	public HashMap<String, Course> getCourses()
+	{
+		return courses;
+	}
+
+	
+	public void setCourses(HashMap<String, Course> courses)
+	{
+		this.courses = courses;
+	}
+
+	
+	public static void main(String[] args) {
+
+		String[] uP = new String[]{ "inputPrefs.csv" };
+		Preferences.parseUserInput( "inputPrefs.csv" );
+		Parser parser = new Parser(Track.SECURITY);
+		parser.parseAll();
+
+		System.out.println( "Main in Scheduler.java");
+		AScheduler s = new AScheduler();// req );
+		
+		LinkedList<ASemester> test = new LinkedList<ASemester>();
+		test = s.uniformCostSearch();
+
+		ScheduleDisplay frame = new ScheduleDisplay();
+		
+//		frame.giveSchedule( test, 1 );
+		
+	}
+	
+} 
---
+++ src/ASemester.java
@@ -0,0 +1,455 @@
+/**
+ * Authors: Abdullah Al-Syed, Sam Friedman, Tim Waterman, Martin Wren
+ * Date: 7/3/14
+ * 
+ * Title: ASemester.java
+ * 
+ *
+ * ASemester here refers to an object containing a proposed semester schedule
+ * For instance, a proposed schedule for Fall 2014 would be 
+ * considered an instance of ASemester. A different proposed schedule
+ * for Fall 2014 would be a different ASemester instance.
+ * 
+ * The semester objects we create are the nodes in our tree traversal.
+ * 
+ * A semester object node chosen for expansion at max depth is considered to be 
+ * in the goal state. Invalid paths are never generated (by checking 
+ * for validity in the child generator method herein).
+ * 
+ * A node checks the utility (implemented as path cost) of its potential
+ * children before actually creating them.  It reports the children 
+ * generated, and their path costs, to AScheduler, which maintains a 
+ * priority queue of the semester object nodes to expand next.
+ * 
+ */
+import java.util.*;
+
+public class ASemester implements Comparable<ASemester> {
+	/*
+	 * CONSTANTS
+	 */
+	// the maximum number of courses a semester schedule can have
+	// this is the max allowed by the system for MS students in CS
+	// protected static final int MAX_SIZE = 4;
+
+	/*
+	 * INSTANCE VARIABLES
+	 */
+
+	// depth = distance from origin semester
+	private int depth;
+
+	/*
+	 * semesterID is an int that is defined as follows: for fall, semesterID = 0
+	 * for spring, semesterID = 1 this system does not handle summer semesters
+	 * for now
+	 */
+	private int semesterID;
+	private int semesterYear;
+	private int term;
+
+	private int nextSemesterID;
+	private int nextSemesterYear;
+
+	public boolean explored = false;
+	public int attempts = 0;
+
+	private ArrayList<ASemester> children;
+
+	// sections refers to the current semester courses being taken at this node
+	private Set<Section> sections;
+	// private Section section;
+
+	// private ASemester
+
+	// a semester that has the number of courses dictated for the term
+	private ASemester completedSemester;
+
+	// previous semester aka parent in the tree
+	private ASemester parentSemester;
+	static Set<Set<Section>> combinationSets;
+
+	private Integer id;
+	private static Integer nodeID = 0;
+
+	// a set to pass to our children so they can keep track of the courses
+	// they have completed in their traversal from root
+	private Set<Course> childrenSemestersInheritedCourses;
+	private Set<Course> inheritedCourses;
+	private Set<Section> poolOfCoursesForChildSemesters;
+	private double utility;
+	private double hvalue;
+	
+	protected static Random r = new Random();
+	private double rangeMin = -0.00001;
+	private double rangeMax = 0.00001;
+	
+	/*
+	 * CONSTRUCTORS
+	 */
+	public ASemester(int depth, int term, int sID, int sYear, Set<Section> sect,
+			ASemester pSemester, Set<Course> iCourses, double inheritedPathCost, 
+			double hX )
+
+	{
+		combinationSets = new HashSet<Set<Section>>();
+		this.depth = depth;
+		this.term = term;
+		this.semesterID = sID;
+		this.semesterYear = sYear;
+		this.sections = sect;
+		this.parentSemester = pSemester;
+		this.inheritedCourses = iCourses;
+		this.utility = inheritedPathCost;
+		this.hvalue = hX;
+		this.children = new ArrayList<ASemester>();
+
+		this.id = nodeID;
+		nodeID++;
+
+		if (AScheduler.semesterBreakpoints.contains(Integer.valueOf(depth + 1))) {
+
+			// if current semester is a fall semester
+			if (semesterID == 0) {
+				nextSemesterID = 1;
+				nextSemesterYear = semesterYear + 1;
+			}
+			// else if current semester is a spring semester
+			else if (semesterID == 1) {
+				nextSemesterID = 0;
+				nextSemesterYear = semesterYear;
+			}
+		} else {
+
+			nextSemesterID = this.semesterID;
+			nextSemesterYear = semesterYear;
+		}
+	}
+
+	/*
+	 * METHODS
+	 */
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public int getSemesterID() {
+		return semesterID;
+	}
+
+	public void setSemesterID(int semesterID) {
+		this.semesterID = semesterID;
+	}
+
+	public int getSemesterYear() {
+		return semesterYear;
+	}
+
+	public void setSemesterYear(int semesterYear) {
+		this.semesterYear = semesterYear;
+	}
+
+	public Set<Section> getSections() {
+		return sections;
+	}
+
+	public ASemester getParentSemester() {
+		return parentSemester;
+	}
+
+	public void setParentSemester(ASemester p) {
+		parentSemester = p;
+	}
+
+	// returns all courses taken by ancestors up until the root
+	public Set<Course> getInheritedCourses() {
+		return inheritedCourses;
+	}
+
+	public void setInheritedCourses(Set<Course> inheritedCourses) {
+		this.inheritedCourses = inheritedCourses;
+	}
+
+	public Set<Section> getPoolOfCoursesForChildSemesters() {
+		return poolOfCoursesForChildSemesters;
+	}
+
+	public double getHvalue() {
+		return hvalue;
+	}
+	
+	public double getUtility() {
+		return utility;
+	}
+
+
+	// returns semester name as a string
+	public String getSemesterName() {
+		String semesterName = "";
+		if (semesterID == 0) {
+			semesterName += "FALL ";
+		} else if (semesterID == 1) {
+			semesterName += "SPRING ";
+		}
+
+		semesterName += semesterYear;
+		return semesterName;
+	}
+
+	// returns string representation of semester state
+	public String toString() {
+		String semesterString = getSemesterName();
+
+		if (sections != null) {
+			for (Section s : sections) {
+				semesterString += "," + s;
+			}
+		}
+
+		semesterString += " at depth " + depth + " with cost " + utility + " h(x)="+hvalue;
+
+		return semesterString;
+	}
+
+	public int getTerm() {
+		return term;
+	}
+
+	// return a linked list for display
+	public LinkedList<ASemester> getFinal() {
+
+		LinkedList<ASemester> rv = new LinkedList<ASemester>();
+
+		if (depth == 0) {
+			return rv;
+		}
+
+		if (depth == Preferences.prefs.getTotalCourses()
+				- Preferences.prefs.coursesTaken.size()) {
+			rv = getParentSemester().getFinal();
+
+			if (term != getParentSemester().getTerm()) {
+				rv.addLast(getParentSemester());
+			}
+			rv.addLast(this);
+
+			return rv;
+		}
+
+		if (term != getParentSemester().getTerm()) {
+
+			if (depth == 1) {
+				return rv;
+			}
+			rv = getParentSemester().getFinal();
+
+			rv.addLast(getParentSemester());
+			return rv;
+		} else {
+			rv = getParentSemester().getFinal();
+		}
+		return rv;
+	}
+
+	// add a new child
+	public ArrayList<ASemester> addChild() {
+
+		ArrayList<ASemester> retVal = new ArrayList<ASemester>();
+
+		Set<Course> childsInheritance = new HashSet<Course>();
+
+		int childTerm = term;
+
+		// if our next course would start a new semester, we need to know now
+		if (AScheduler.semesterBreakpoints.contains(Integer.valueOf(depth + 1))
+				|| term == -1) {
+
+			// this is considering classes from a new term. if the complete
+			// offerings of this term and subsequent terms could not complete
+			// the track, remove this node.
+			LinkedList<Course> validityCheckList = new LinkedList<Course>();
+			for (int i = term + 1; i < Preferences.prefs.getNumSems(); i++) {
+				for (Section candidate : AScheduler.directoryOfClasses.get(i)) {
+					validityCheckList.add(sectionsToCourses(candidate));
+				}
+			}
+			// add sections taken this semester
+			if (sections != null)
+				for (Section s : sections) {
+					validityCheckList.add(sectionsToCourses(s));
+				}
+			// add inherited courses from previous semesters
+			for (Course c : inheritedCourses) {
+				validityCheckList.add(c);
+			}
+
+			ArrayList<Integer> reqScore = new ArrayList<Integer>();
+			reqScore = Requirements.rulesUnmet(validityCheckList);
+
+			if (reqScore.get(1) > 0) {
+				// We can never end up valid
+				return retVal;
+			}
+
+			validityCheckList.clear();
+
+			for (Course c : childsInheritance) {
+				if (c != null) {
+					validityCheckList.add(c);
+				}
+			}
+
+			childTerm = term + 1;
+			if (sections != null) {
+				for (Course c : sectionsToCourses(sections)) {
+					childsInheritance.add(c);
+				}
+			}
+		}
+
+		// no matter what happens, add previous semester courses to the
+		// next semester's lead course
+		for (Course c : inheritedCourses) {
+			childsInheritance.add(c);
+		}
+
+		// find the section offerings for this term
+
+		// build a list for holding possible children
+		ArrayList<ArrayList<Section>> possibleSemesters = new ArrayList<ArrayList<Section>>();
+
+		for (Section candidate : AScheduler.directoryOfClasses.get(childTerm)) {
+
+			// already defined for the current semester
+			if (sections != null) {
+				if (sections.contains(candidate)) {
+					continue;
+				}
+			}
+
+			// already defined in previous semesters
+			if (inheritedCourses.contains(sectionsToCourses(candidate))) {
+				continue;
+			}
+
+			ArrayList<Section> newPossibility = new ArrayList<Section>();
+			if (sections != null && term == childTerm) {
+				newPossibility.addAll(sections);
+			}
+			newPossibility.add(candidate);
+
+			// determine the utility of the child
+			double childUtility = Utility.getUtility(newPossibility) + 
+					rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+			
+
+			// add the courses we're considering to our inherited courses in a
+			// LL
+			LinkedList<Course> validityCheckList = new LinkedList<Course>();
+			for (Course c : childsInheritance) {
+				if (c != null) {
+					validityCheckList.add(c);
+				}
+
+			}
+			for (Section s : newPossibility) {
+				validityCheckList.add(s.getParent());
+			}
+
+			// if the rules not met by these courses is greater than the total
+			// number
+			// of courses we can choose in subsequent semesters, we cannot
+			// complete
+			// the degree as requested and will not create the child node.
+
+			ArrayList<Integer> reqScore = new ArrayList<Integer>();
+			reqScore = Requirements.rulesUnmet(validityCheckList);
+
+			if (reqScore.get(1) > (Preferences.prefs.getTotalCourses()
+					+ Preferences.prefs.coursesTaken.size() - validityCheckList
+						.size())) {
+
+				// We can never end up valid
+				continue;
+
+			}
+
+			// set the value of our heuristic function
+			hvalue = utility + childUtility + ( reqScore.get(0) *
+					250 );
+					
+					/*
+					utility + childUtility + ( reqScore.get(1) +
+					( reqScore.get(0) / ( Preferences.prefs.getTotalCourses()
+							- Preferences.prefs.coursesTaken.size() - ( depth + 1 ) )
+							* 1000 ) / ( depth + 1 ) );
+			*/
+					
+					//( reqScore.get(0) * 15 );
+
+
+			Set<Section> childSections = new HashSet<Section>();
+
+			if (sections != null && term == childTerm) {
+				childSections.addAll(sections);
+			}
+
+			childSections.add(candidate);
+
+			ASemester newChild = new ASemester(depth + 1, childTerm,
+					nextSemesterID, nextSemesterYear, childSections, this,
+					childsInheritance, utility + childUtility, hvalue );
+
+			retVal.add(newChild);
+
+		}
+
+		return retVal;
+	}
+	
+	
+
+	// convert a set of sections to a set of courses
+	private static Set<Course> sectionsToCourses(Set<Section> givenSet) {
+
+		if (givenSet == null) {
+			return null;
+		}
+
+		Set<Course> retVal = new HashSet<Course>();
+
+		for (Section s : givenSet) {
+			retVal.add(s.getParent());
+		}
+
+		return retVal;
+	}
+
+	// convert a sections to a course
+	private static Course sectionsToCourses(Section givenSet) {
+
+		if (givenSet == null) {
+			return null;
+		}
+
+		return givenSet.getParent();
+	}
+
+
+	
+	@Override
+	public int compareTo(ASemester s) {
+		if (this.hvalue < s.getHvalue()) {
+			return -1;
+		}
+		return 1;
+
+	}
+
+	public double getPathUtility() {
+		return this.utility;
+	}
+
+	
+}
