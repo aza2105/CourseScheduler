892604a20e25b2e0144431e6d2d719e10797c42f
Tim Waterman <watermantium@gmail.com>
2014-07-04 14:47:43 -0400

    GA commented for clarity

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -27,7 +27,7 @@ public class GeneticAlgorithm {
 	int numSems = 0; //number of semesters we need to go
 	int currentSem = 0; //the index of the current semester
 	ArrayList<PriorityQueue<Chromosome>> allGens = new ArrayList<PriorityQueue<Chromosome>>();
-	ArrayList<Chromosome> testSet = new ArrayList<Chromosome>();
+	ArrayList<Chromosome> testSet = new ArrayList<Chromosome>(); //final set to test validity with
 	
 	public GeneticAlgorithm() {
 		
@@ -47,15 +47,15 @@ public class GeneticAlgorithm {
 		for(int i = 0; i < numSems; i++) {
 			
 			population = new PriorityQueue<Chromosome>();
-			if (i == 0)
+			if (i == 0) //if this is the first semester, proceed with known courses
 				run(GENERATIONS, current, Preferences.prefs.numCoursesPerSem[i]);
 			else
 				run(GENERATIONS, prior, Preferences.prefs.numCoursesPerSem[i]);
 			
-			allGens.add(population);
+			allGens.add(population); //keep all the populations inside allGens
 		}
 		
-		//now cycle through allgens and remove all the chromosomes that have duplicates in them
+		//now cycle through allGens and remove all the chromosomes that have duplicates in them
 		
 		while( !allGens.get(0).isEmpty() ) {
 			
@@ -87,7 +87,7 @@ public class GeneticAlgorithm {
 		
 		System.out.println("size after PRUNE : " + testSet.size());
 		
-		ArrayList<Section> ultraList = new ArrayList<Section>();
+		ArrayList<Section> ultraList = new ArrayList<Section>(); //collection of every course
 		ultraList.addAll(current);
 		ultraList.addAll(prior);
 		
@@ -107,16 +107,6 @@ public class GeneticAlgorithm {
 			
 		}
 
-		
-		//finalRun(GENERATIONS,testSet,10);
-		/*
-		testSet.clear();
-
-		int size = population.size();
-		for(int i = 0; i < size; i++) {
-			testSet.add(population.poll());
-		}
-		*/
 		System.out.println("Final test set size: " + testSet.size());
 		
 		//Now lets test to see how many schedules this thing might have completed
@@ -141,83 +131,19 @@ public class GeneticAlgorithm {
 		
 	}//end GA constructor
 	
-	public void finalRun(int gen, ArrayList<Chromosome> list, int semlen) {
-		
-		population.clear();
-		
-		for(int i = 0; i < list.size(); i++) {
-			population.add(list.get(i));
-		}
-		
-		int i = 0;
-		
-		ArrayList<Chromosome> pool;
-		ArrayList<Chromosome> toBreed;
-		
-		while(i < gen) {
-			
-			pool = new ArrayList<Chromosome>();
-			toBreed = new ArrayList<Chromosome>();
-			
-			int POPMAX = population.size();
-			
-			for(int j = 0; j < POPMAX; j++) {
-				pool.add(population.poll());
-			}
-
-			Random generator = new Random();
-			int prob = generator.nextInt(pool.size());
-			for(int j = 0; j < pool.size(); j++) {
-				
-				if (j < prob) {
-					toBreed.add(pool.get(j));
-				}	
-			}//end for
-			
-			if(toBreed.size() < 2) {
-				
-				for(int j = 0; j < POPMAX; j++) {
-					population.add(pool.get(j));
-				}
-				
-				i++;
-				continue;
-			}
-			
-			int breedingSize = toBreed.size();
-			for(int j = 1; j < breedingSize; j+=2) {
-				population.add( breed(toBreed.get(j - 1),toBreed.get(j), semlen) );
-			}
-			
-			for(int j = 0; j < POPMAX; j++) {
-				population.add(pool.get(j));
-			}
-			
-			i++;
-			
-			//EVERY FEW GENERATIONS NEED TO CULL SOME OF THE SAMPLE
-			if(population.size() > 1000)
-				cull(population);
-			
-
-			//mutate(population);
-		}
-		
-	}
-
-	
+	//run the algorithm on a given section list
 	public void run(int gen, ArrayList<Section> current, int semlen) {
 		
 		
-		randomize(current);
-		establish(current, semlen);
+		randomize(current); //randomize the order that the sections are in
+		establish(current, semlen); //establish the population to chromosome length of semlen
 
 		//Population is now set up, time to select for breeding
 		
 		int i = 0;
 		
-		ArrayList<Chromosome> pool;
-		ArrayList<Chromosome> toBreed;
+		ArrayList<Chromosome> pool; //overall pool
+		ArrayList<Chromosome> toBreed; //lucky ones to breed this generation
 		
 		while (i < gen) {
 			
@@ -229,18 +155,16 @@ public class GeneticAlgorithm {
 			for(int j = 0; j < POPMAX; j++) {
 				pool.add(population.poll());
 			}
-			
-			double step = 0.98;
-			Random generator = new Random();
+
 			int prob = pool.size();
-			for(int j = 0; j < pool.size(); j++) {
+			for(int j = 0; j < pool.size(); j++) { //having troubles here. Just breed everything for now
 				
 				if (j < prob) {
 					toBreed.add(pool.get(j));
 				}	
 			}//end for
 			
-			if(toBreed.size() < 2) {
+			if(toBreed.size() < 2) { //if we don't have enough to breed, don't
 				
 				for(int j = 0; j < POPMAX; j++) {
 					population.add(pool.get(j));
@@ -271,6 +195,8 @@ public class GeneticAlgorithm {
 		
 	}
 	
+	//mutation currently not working the way I would like it to
+	//Shouldn't be called
 	public void mutate(PriorityQueue<Chromosome> pop) {
 		
 		ArrayList<Chromosome> hold = new ArrayList<Chromosome>();
@@ -302,6 +228,7 @@ public class GeneticAlgorithm {
 		}
 	}
 	
+	//cull the bottom half of the population
 	public void cull(PriorityQueue<Chromosome> pop) {
 		
 		int num = pop.size();
@@ -387,9 +314,11 @@ public class GeneticAlgorithm {
 		
 	}//end randomize method
 	
+	//creates the offspring
+	//also, father & mother are just distinct variable names. Marriage equality all the way
 	public Chromosome breed(Chromosome father, Chromosome mother, int len) {
 
-		if (len > 5) {
+		if (len > 5) { //if the size is big enough I want to interleave them
 
 			Section[] child = new Section[len];
 			
@@ -406,6 +335,7 @@ public class GeneticAlgorithm {
 			return x;
 				
 		}
+		//otherwise I'll cut it in half and take half of each schedule
 		int cutoff = len / 2; //half the length
 		
 		Section[] stepMother;
@@ -437,7 +367,6 @@ public class GeneticAlgorithm {
 	}
 	
 	
-
 	
 	//PRIVATE CLASS FOR PARSE TESTING
 	private class Parse {
